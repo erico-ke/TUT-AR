4d7056b980514afe7141cd297a4e66d52c16fba9
erico-ke <eliasricokergaravat@gmail.com>
2024-12-14 13:43:05 +0100

    MainPage update

--- tuto_ar/lib/pages/main_page.dart
+++ tuto_ar/lib/pages/main_page.dart
@@ -3,6 +3,7 @@ import 'package:tuto_ar/pages/search_page.dart';
 import 'package:tuto_ar/pages/user_page.dart';
 
 final List<int> numbers = [1, 2, 3, 4, 5];
+final List<String> names = ['Arduino Basics 101', 'Changing a Tire', 'How to cook risotto', 'How to change a diaper', 'Giving your Laptop a new Ram'];
 
 class MainPage extends StatelessWidget {
   const MainPage({super.key});
@@ -20,30 +21,53 @@ class MainPage extends StatelessWidget {
                 alignment: Alignment.topCenter,
                 child: Text('Continue learning!'),
               ),
-              Container(
-                padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 20.0),
-                height: MediaQuery.of(context).size.height / 1.3,
-                child: ListView.builder(
+              Align(
+                alignment: Alignment.centerLeft,
+                child: Container(
+                  padding:
+                      EdgeInsets.symmetric(horizontal: 16.0, vertical: 20.0),
+                  height: MediaQuery.of(context).size.height / 1.3,
+                  width: MediaQuery.of(context).size.width,
+                  child: ListView.builder(
                     scrollDirection: Axis.vertical,
                     itemCount: numbers.length,
                     itemBuilder: (context, index) {
                       return Container(
-                      width: MediaQuery.of(context).size.width / 1.2,
-                      margin: EdgeInsets.symmetric(vertical: 10.0),
-                      child: Card(
-                        color: Colors.blue,
-                        child: Container(
-                        height: MediaQuery.of(context).size.height / 6,
-                        child: Center(
-                          child: Text(
-                          numbers[index].toString(),
-                            style: TextStyle(
-                            color: Colors.white, fontSize: 36.0),
-                        )),
+                        width: MediaQuery.of(context).size.width,
+                        margin: EdgeInsets.symmetric(vertical: 10.0),
+                        child: Row(
+                          children: [
+                            Expanded(
+                              flex: 3,
+                              child: Card(
+                                color: Colors.blue,
+                                child: Container(
+                                  height:
+                                      MediaQuery.of(context).size.height / 6,
+                                  child: Center(
+                                    child: Text(
+                                      numbers[index].toString(),
+                                      style: TextStyle(
+                                          color: Colors.white, fontSize: 36.0),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
+                            SizedBox(width: 16),
+                            Expanded(
+                              flex: 2,
+                              child: Text(
+                                '${names[index]}',
+                                style: TextStyle(fontSize: 18.0),
+                              ),
+                            ),
+                          ],
                         ),
-                      ),
                       );
-                    }),
+                    },
+                  ),
+                ),
               ),
             ],
           ),
