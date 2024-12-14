fb8f0b15f9bd4c6c2e12e29eab95b2ed28081e93
erico-ke <eliasricokergaravat@gmail.com>
2024-12-15 00:14:08 +0100

    Coffee

--- tuto_ar/lib/pages/search_result.dart
+++ tuto_ar/lib/pages/search_result.dart
@@ -1,55 +1,77 @@
 import 'package:flutter/material.dart';
 import 'package:tuto_ar/pages/bookmark_page.dart';
-import 'package:tuto_ar/pages/main_page.dart';
+import 'package:tuto_ar/pages/search_page.dart';
 import 'package:tuto_ar/pages/user_profile_page.dart';
-import 'package:tuto_ar/pages/search_result.dart';
+import 'package:tuto_ar/pages/main_page.dart';
 
 
-class SearchPage extends StatelessWidget {
-  const SearchPage({super.key});
+final List<String> names = ['Coffee making Tutorial', 'How to make coffee', 'The coffee final guide', "How to make a coffee for beginners"];
+final List<Image> numbers = [
+  Image.asset('cofe.jpeg'),
+  Image.asset('coffe.png'),
+  Image.asset('coffee.jpeg'),
+  Image.asset('coofe.jpeg'),
+];
+
+class SearchResult extends StatelessWidget {
+  const SearchResult({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(),
-        body: Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Row(
-              children: [
-                // Barra de búsqueda
-                Expanded(
-                  child: TextField(
-                    decoration: InputDecoration(
-                      hintText: 'Buscar...',
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(12.0),
+      appBar: AppBar(
+        title: const Text('Search Results: '),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            Align(
+              alignment: Alignment.centerLeft,
+              child: Container(
+                padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 20.0),//Cuando testeemos en Movil, ajustar los tamaños.
+                height: MediaQuery.of(context).size.height / 1.3,
+                width: MediaQuery.of(context).size.width,
+                child: ListView.builder(
+                  scrollDirection: Axis.vertical,
+                  itemCount: numbers.length,
+                  itemBuilder: (context, index) {
+                    return Container(
+                      width: MediaQuery.of(context).size.width,
+                      margin: EdgeInsets.symmetric(vertical: 10.0),
+                      child: Row(
+                        children: [
+                          Expanded(
+                            flex: 3,
+                            child: Card(
+                              color: const Color.fromARGB(255, 255, 255, 255),
+                              child: Container(
+                                height: MediaQuery.of(context).size.height / 6,
+                                child: Center(
+                                  child: numbers[index],
+                                ),
+                              ),
+                            ),
+                          ),
+                          SizedBox(width: 16),
+                          Expanded(
+                            flex: 2,
+                            child: Text(
+                              '${names[index]}',
+                              style: TextStyle(fontSize: 18.0),
+                            ),
+                          ),
+                        ],
                       ),
-                    ),
-                  ),
-                ),
-                const SizedBox(width: 8), // Espacio entre la barra y el botón
-                // Botón de lupa
-                ElevatedButton(
-                  onPressed: () {
-                    // Acción al presionar el botón
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => SearchResult()),
                     );
                   },
-                  style: ElevatedButton.styleFrom(
-                    padding: const EdgeInsets.all(14.0), // Tamaño del botón
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(8.0),
-                    ),
-                  ),
-                  child: const Icon(Icons.search),
                 ),
-              ],
-            )),
+              ),
+            ),
+          ],
+        ),),
         bottomNavigationBar: Container(
             color: const Color(0xffC7F9CC),
-            height: 70.0,
+            height: 70.0, 
             child: Row(children: [
               Container(
                 height: 70.0,
@@ -65,17 +87,17 @@ class SearchPage extends StatelessWidget {
                       ),
                     ),
                   ),
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => MainPage()),
-                    );
-                  },
-                  child: Icon(
-                    Icons.home,
-                  ),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => MainPage()),
+                  );
+                },
+                child: Icon(
+                  Icons.home,
+                 
                 ),
-              ),
+              ),),
               Container(
                 width: MediaQuery.of(context).size.width / 4,
                 height: 70.0,
@@ -90,17 +112,16 @@ class SearchPage extends StatelessWidget {
                       ),
                     ),
                   ),
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => SearchPage(),
-                      ),
-                    );
-                  },
-                  child: Icon(color: const Color(0xff22577A), Icons.search),
-                ),
-              ),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => SearchPage(),
+                    ),
+                  );
+                },
+                child: Icon( color: const Color(0xff22577A), Icons.search),
+              ),),
               Container(
                 width: MediaQuery.of(context).size.width / 4,
                 height: 70.0,
@@ -148,8 +169,11 @@ class SearchPage extends StatelessWidget {
                     );
                   },
                   child: Icon(Icons.person),
-                ),
-              ),
-            ])));
+                  ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
